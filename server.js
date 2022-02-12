97e4d98afbc6bfc7231e210d457d15533592b440
Jaabel <Jaabel@outlook.com>
2022-02-12 14:46:01 -0600

    converted MONGO_URI to a string in .env

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ dotenv.config();
 /** config mongoDB */
 const mongoose = require('mongoose')
 mongoose.connect(
-  process.env.MONGO_URL,
+  process.env.MONGO_URI,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log("🕹️  DB connected 🛡️")
 );
