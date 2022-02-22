563a9daefd33c8bc34a193e71beb1e1ee0f5c7c7
Jaabel <Jaabel@outlook.com>
2022-02-21 19:21:05 -0600

    debugging build conflict

--- server.js
+++ server.js
@@ -66,10 +66,11 @@ app.use((error, req, res, next) => {
 
 // Serve static assets if in production
 // Set static folder
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
+  app.get('/', (req, res, next) => res.sendFile(path.join(__dirname+'client/build/index.html')))
   app.get('*', (req, res, next) => res.sendFile(path.join(__dirname+'/client/build/index.html')))
-// }
+}
 
 // starts the server
 const server = app.listen(port, () => {
